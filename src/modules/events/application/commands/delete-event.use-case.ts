662b64c29c985d35a61e78185bb86b390efa3d70
Liza-Khube <elisekhu@gmail.com>
2026-04-26 23:54:28 +0300

    feat(events): implement event use cases

---
+++ src/modules/events/application/commands/delete-event.use-case.ts
@@ -0,0 +1,26 @@
+import { IEventRepository } from "../../domain/repositories/event.repository.interface";
+import { NotFoundError, UnauthorizedError } from "../../../../shared/domain/errors/domain.error";
+
+export interface DeleteEventCommand {
+  eventId: number;
+  requestingUserId: string;
+}
+
+export class DeleteEventUseCase {
+  constructor(private readonly eventsRepository: IEventRepository) {}
+
+  async execcute(command: DeleteEventCommand): Promise<void> {
+    const event = await this.eventsRepository.findById(command.eventId);
+    if (!event) {
+      throw new NotFoundError(`Event ${command.eventId} not found`);
+    }
+    if (!event.isOwnedBy(command.requestingUserId)) {
+      throw new UnauthorizedError(`User is not the owner of the event ${command.eventId}`);
+    }
+    if (!event.isCancelledOrFinished()) {
+      throw new Error(`Event ${command.eventId} cannot be deleted in its current state`);
+    }
+
+    await this.eventsRepository.delete(event.id);
+  }
+}
