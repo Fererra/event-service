a4b57afb4ec6b7cbacdbc49387de584be32c2e55
Stas Oleksiichuk <stas.oleksiichuk88@gmail.com>
2026-04-29 17:39:40 +0300

    feat(venue): add searching methods to venue repository (domain)

--- src/modules/venue/domain/repositories/venue.repository.ts
+++ src/modules/venue/domain/repositories/venue.repository.ts
@@ -3,4 +3,6 @@ import { Venue } from "../entities/venue.entity";
 export interface VenueRepository {
   save(venue: Venue): Promise<void>;
   findByAddress(address: string): Promise<Venue | null>;
+  findAll(): Promise<Venue[]>;
+  findById(id: string): Promise<Venue | null>;
 }
