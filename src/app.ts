861611ae37fe690f3b9566289e51914fff9b21b0
Liza-Khube <elisekhu@gmail.com>
2026-04-29 22:58:17 +0300

    fix(server): fix username in db config

--- src/app.ts
+++ src/app.ts
@@ -51,7 +51,7 @@ async function bootstrap() {
     db: {
       host: process.env.DB_HOST || "localhost",
       port: Number(process.env.DB_PORT) || 5432,
-      username: process.env.DB_USER || "postgres",
+      username: process.env.DB_USERNAME || "postgres",
       password: process.env.DB_PASSWORD || "postgres",
       database: process.env.DB_NAME || "auth_db",
     },
