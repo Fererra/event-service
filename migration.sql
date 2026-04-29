7a26b8ca9d64ac38ee4405958a24f198e5127b06
Liza-Khube <elisekhu@gmail.com>
2026-04-29 23:00:26 +0300

    feat(database): add creating tables and indexes for events and tickets

--- migration.sql
+++ migration.sql
@@ -16,5 +16,29 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     created_at  TIMESTAMP    NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS events (
+    id              SERIAL PRIMARY KEY,
+    name            VARCHAR(255) NOT NULL,
+    owner_id        UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    description     TEXT NOT NULL,
+    start_timestamp TIMESTAMP NOT NULL,
+    end_timestamp   TIMESTAMP NOT NULL,
+    organisator     VARCHAR(255) NOT NULL,
+    venue_id        INTEGER NOT NULL,
+    status          VARCHAR(50) NOT NULL DEFAULT 'IN_PLANNING',
+    created_at      TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS tickets (
+    id          SERIAL PRIMARY KEY,
+    event_id    INTEGER NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+    type        VARCHAR(50) NOT NULL,
+    "limit"     INTEGER NOT NULL,
+    price       DECIMAL(10, 2) NOT NULL 
+);
+
 CREATE INDEX IF NOT EXISTS idx_refresh_tokens_token_hash ON refresh_tokens(token_hash);
 CREATE INDEX IF NOT EXISTS idx_refresh_tokens_user_id    ON refresh_tokens(user_id);
+
+CREATE INDEX IF NOT EXISTS idx_events_owner_id ON events(owner_id);
+CREATE INDEX IF NOT EXISTS idx_tickets_event_id ON tickets(event_id);
