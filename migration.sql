a438a9cf6ba3a5c1d14b83dbb21da9fd7a6735ec
Liza-Khube <elisekhu@gmail.com>
2026-04-30 19:43:22 +0300

    fix(database): replace creating venues before creating events and tickets in migration file

--- migration.sql
+++ migration.sql
@@ -16,6 +16,14 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     created_at  TIMESTAMP    NOT NULL DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS venues (
+    id          UUID PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    address     VARCHAR(255) NOT NULL UNIQUE,
+    capacity    INT,    
+    created_at  TIMESTAMP    NOT NULL DEFAULT NOW()
+);
+
 CREATE TABLE IF NOT EXISTS events (
     id              SERIAL PRIMARY KEY,
     name            VARCHAR(255) NOT NULL,
@@ -36,15 +44,6 @@ CREATE TABLE IF NOT EXISTS tickets (
     "limit"     INTEGER NOT NULL,
     price       DECIMAL(10, 2) NOT NULL 
 );
-
-CREATE TABLE IF NOT EXISTS venues (
-    id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name        VARCHAR(255) NOT NULL,
-    address     VARCHAR(255) NOT NULL UNIQUE,
-    capacity    INT,    
-    created_at  TIMESTAMP    NOT NULL DEFAULT NOW()
-);
-
 
 CREATE INDEX IF NOT EXISTS idx_refresh_tokens_token_hash ON refresh_tokens(token_hash);
 CREATE INDEX IF NOT EXISTS idx_refresh_tokens_user_id    ON refresh_tokens(user_id);
