e002219bee80766afae91060b74436a0d7ff396f
Stas Oleksiichuk <stas.oleksiichuk88@gmail.com>
2026-05-01 19:06:08 +0300

    feat(registrations): add new types and schemas to dto for GET methods - added dto-mapper for correct response

--- src/modules/registrations/presentation/dto/registration.dto.ts
+++ src/modules/registrations/presentation/dto/registration.dto.ts
@@ -1,8 +1,11 @@
-export interface CreateRegistrationDto {
-  ticket_id: number;
-}
-
-export const createRegistrationSchema = {
+export const CreateRegistrationSchema = {
+  params: {
+    type: "object",
+    required: ["eventId"],
+    properties: {
+      eventId: { type: "integer" },
+    },
+  },
   body: {
     type: "object",
     required: ["ticket_id"],
@@ -10,4 +13,81 @@ export const createRegistrationSchema = {
       ticket_id: { type: "integer", minimum: 1 },
     },
   },
+};
+
+export type CreateRegistrationDto = {
+  Params: {
+    eventId: string;
+  };
+  Body: {
+    ticket_id: number;
+  };
+};
+
+export const GetUserRegistrationsSchema = {
+  params: {
+    type: "object",
+    required: ["userId"],
+    properties: {
+      userId: { type: "string", format: "uuid" },
+    },
+  },
+};
+
+export type GetUserRegistrationsDto = {
+  Params: {
+    userId: string;
+  };
+};
+
+export const GetUserRegistrationSchema = {
+  params: {
+    type: "object",
+    required: ["userId", "registrationId"],
+    properties: {
+      userId: { type: "string", format: "uuid" },
+      registrationId: { type: "string", format: "uuid" },
+    },
+  },
+};
+
+export type GetUserRegistrationDto = {
+  Params: {
+    userId: string;
+    registrationId: string;
+  };
+};
+
+export const GetEventRegistrationsSchema = {
+  params: {
+    type: "object",
+    required: ["eventId"],
+    properties: {
+      eventId: { type: "integer" },
+    },
+  },
+};
+
+export type GetEventRegistrationsDto = {
+  Params: {
+    eventId: number;
+  };
+};
+
+export const GetEventRegistrationSchema = {
+  params: {
+    type: "object",
+    required: ["eventId", "registrationId"],
+    properties: {
+      eventId: { type: "integer" },
+      registrationId: { type: "string", format: "uuid" },
+    },
+  },
+};
+
+export type GetEventRegistrationDto = {
+  Params: {
+    eventId: number;
+    registrationId: string;
+  };
 };
