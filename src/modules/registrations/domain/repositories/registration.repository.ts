408ea6e1ef430bf04cc90150935e88fc341441cd
Stas Oleksiichuk <stas.oleksiichuk88@gmail.com>
2026-05-01 19:02:17 +0300

    feat(registrations): add new methods to registration repository interface

--- src/modules/registrations/domain/repositories/registration.repository.ts
+++ src/modules/registrations/domain/repositories/registration.repository.ts
@@ -3,4 +3,9 @@ import { Registration } from "../entities/registration.entity";
 export interface RegistrationRepository {
   countByTicketId(ticketId: number): Promise<number>;
   save(registration: Registration): Promise<Registration>;
+
+  findByUserId(userId: string): Promise<Registration[]>;
+  findByIdAndUserId(id: string, userId: string): Promise<Registration | null>;
+  findByEventId(eventId: number): Promise<Registration[]>;
+  findByIdAndEventId(id: string, eventId: number): Promise<Registration | null>;
 }
