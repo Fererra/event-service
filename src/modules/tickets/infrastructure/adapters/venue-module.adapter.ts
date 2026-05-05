501914025f330dd6af5f88e6e42a233b03d007fd
Stas Oleksiichuk <stas.oleksiichuk88@gmail.com>
2026-05-06 02:01:31 +0300

    refactor(event/tickets): replace venue use-cases for queries

--- src/modules/tickets/infrastructure/adapters/venue-module.adapter.ts
+++ src/modules/tickets/infrastructure/adapters/venue-module.adapter.ts
@@ -1,10 +1,11 @@
 import { VenueData, IVenueRepository } from "../../domain/repositories/venue.repository.interface";
-import { GetVenueByIdUseCase } from "../../../venue/application/use-cases/get-venue-by-id.use-case";
+import { GetVenueByIdQueryHandler } from "../../../venue/application/queries/get-venue-by-id/get-venue-by-id.handler";
+import { GetVenueByIdQuery } from "../../../venue/application/queries/get-venue-by-id/get-venue-by-id.query";
 
 export class VenueModuleAdapter implements IVenueRepository {
-  constructor(private readonly getVenueUseCase: GetVenueByIdUseCase) {}
+  constructor(private readonly getVenueUseCase: GetVenueByIdQueryHandler) {}
   async findById(id: string): Promise<VenueData | null> {
-    const venue = await this.getVenueUseCase.execute(id);
+    const venue = await this.getVenueUseCase.handle(new GetVenueByIdQuery(id));
     if (!venue) return null;
     return {
       id: venue.id,
