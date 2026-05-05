e644c4baa73b21c4e5c6f22d605443c8ab8f2b72
Liza-Khube <elisekhu@gmail.com>
2026-05-05 23:53:22 +0300

    refactor(tickets): update files for command-query separartion

--- src/modules/tickets/application/queries/get-event-tickets.use-case.ts
+++ src/modules/tickets/application/queries/get-event-tickets.use-case.ts
@@ -1,19 +1,19 @@
-import { Ticket } from "../../domain/entities/ticket.entity";
-import { ITicketRepository } from "../../domain/repositories/ticket.repository.interface";
-import { IEventLookupRepository } from "../../domain/repositories/event-lookup.repository.interface";
+import { TicketReadModel } from "./ticket.read-model";
+import { ITicketReadRepository } from "./ticket-read.repository.interface";
+import { IEventReadRepository } from "../../../events/application/queries/event-read.repository.interface";
 import { NotFoundError } from "../../../../shared/domain/errors/domain.error";
 
 export class GetEventTicketsUseCase {
   constructor(
-    private readonly ticketRepository: ITicketRepository,
-    private readonly eventLookupRepository: IEventLookupRepository,
+    private readonly ticketReadRepository: ITicketReadRepository,
+    private readonly eventLookupRepository: IEventReadRepository,
   ) {}
 
-  async execute(eventId: number): Promise<Ticket[]> {
+  async execute(eventId: number): Promise<TicketReadModel[]> {
     const event = await this.eventLookupRepository.findById(eventId);
     if (!event) {
       throw new NotFoundError(`Event with id ${eventId} not found`);
     }
-    return this.ticketRepository.findByEventId(eventId);
+    return this.ticketReadRepository.findByEventId(eventId);
   }
 }
