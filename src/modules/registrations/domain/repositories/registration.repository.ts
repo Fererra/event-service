c516d780d46075cf6e1dda8268d306264d0110fd
Stas Oleksiichuk <stas.oleksiichuk88@gmail.com>
2026-05-06 07:14:36 +0300

    fix(registrations): change amount of parameters in countByTicketId function in repository interface

--- src/modules/registrations/domain/repositories/registration.repository.ts
+++ src/modules/registrations/domain/repositories/registration.repository.ts
@@ -3,6 +3,6 @@ import { Registration } from "../entities/registration.entity";
 export interface RegistrationRepository {
   save(registration: Registration): Promise<Registration>;
   findById(id: string): Promise<Registration | null>;
-  countByTicketId(eventId: number, ticketId: number): Promise<number>;
+  countByTicketId(ticketId: number): Promise<number>;
   delete(id: string): Promise<void>;
 }
