9256cc6a8f12a9eb52a0486add31e81363b98942
Liza-Khube <elisekhu@gmail.com>
2026-05-11 18:22:26 +0300

    refactor(events): update adapter imports

--- src/modules/events/infrastructure/adapters/venue-module.adapter.ts
+++ src/modules/events/infrastructure/adapters/venue-module.adapter.ts
@@ -1,11 +1,10 @@
 import { VenueData, IVenueRepository } from "../../domain/repositories/venue.repository.interface";
-import { GetVenueByIdQueryHandler } from "../../../venue/application/queries/get-venue-by-id/get-venue-by-id.handler";
-import { GetVenueByIdQuery } from "../../../venue/application/queries/get-venue-by-id/get-venue-by-id.query";
+import { VenueApi } from "../../../venue/api/venue.api";
 
 export class VenueModuleAdapter implements IVenueRepository {
-  constructor(private readonly getVenueUseCase: GetVenueByIdQueryHandler) {}
+  constructor(private readonly venueModule: VenueApi) {}
   async findById(id: string): Promise<VenueData | null> {
-    const venue = await this.getVenueUseCase.handle(new GetVenueByIdQuery(id));
+    const venue = await this.venueModule.getVenueById(id);
     if (!venue) return null;
     return {
       id: venue.id,
