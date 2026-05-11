379e6645680e3b84000923bbeb8033e4e43408ef
Stas Oleksiichuk <stas.oleksiichuk88@gmail.com>
2026-05-11 06:55:06 +0300

    refactor(registration): refactor cancel handler with eventBus - removed unnecessary parameter and its checking

--- src/modules/registrations/application/commands/cancel-registration/cancel-registration.command.ts
+++ src/modules/registrations/application/commands/cancel-registration/cancel-registration.command.ts
@@ -3,7 +3,6 @@ import { UserRole } from "../../../../../shared/domain/value-objects/user-role.e
 export class CancelRegistrationCommand {
   constructor(
     public readonly registrationId: string,
-    public readonly targetUserId: string,
     public readonly executorId: string,
     public readonly executorRole: UserRole,
   ) {}
