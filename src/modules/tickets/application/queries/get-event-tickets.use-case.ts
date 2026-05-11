3c14aede74242467f98eda7d1a57cc2ac4ab4802
Liza-Khube <elisekhu@gmail.com>
2026-05-11 12:41:45 +0300

    refactor(tickets): update event repo import

--- src/modules/tickets/application/queries/get-event-tickets.use-case.ts
+++ src/modules/tickets/application/queries/get-event-tickets.use-case.ts
@@ -1,12 +1,12 @@
 import { TicketReadModel } from "./ticket.read-model";
 import { ITicketReadRepository } from "./ticket-read.repository.interface";
-import { IEventReadRepository } from "../../../events/application/queries/event-read.repository.interface";
+import { IEventLookupRepository } from "../../domain/repositories/event-lookup.repository.interface";
 import { NotFoundError } from "../../../../shared/domain/errors/domain.error";
 
 export class GetEventTicketsUseCase {
   constructor(
     private readonly ticketReadRepository: ITicketReadRepository,
-    private readonly eventLookupRepository: IEventReadRepository,
+    private readonly eventLookupRepository: IEventLookupRepository,
   ) {}
 
   async execute(eventId: number): Promise<TicketReadModel[]> {
